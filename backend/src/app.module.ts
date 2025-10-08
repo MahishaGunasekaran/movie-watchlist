96ce7081d872961db7b522240cb7aaffb0a60598
Mahi-Mani <m.mahishamani@gmail.com>
2025-10-07 22:01:26 -0700

    Setup backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
+import { FavoritesModule } from './favorites/favorites.module';
+
+@Module({
+  imports: [MoviesModule, FavoritesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
