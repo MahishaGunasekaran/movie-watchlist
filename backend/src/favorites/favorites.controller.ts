e6e02389990cf63b9d7cc07b8fd2e961a907309c
Mahi-Mani <m.mahishamani@gmail.com>
2025-10-07 23:03:36 -0700

    Add backend routes for favorites module

--- backend/src/favorites/favorites.controller.ts
+++ backend/src/favorites/favorites.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
+import { FavoritesService } from './favorites.service';
+import type { Movie } from '../types/movie.type';
 
 @Controller('favorites')
-export class FavoritesController {}
+export class FavoritesController {
+    constructor(private readonly favoritesService: FavoritesService) { }
+
+    @Post(":movie")
+    addFavorite(@Body() movie: Movie): string {
+        this.favoritesService.addFavorite(movie);
+        return 'Movie added to favorites';
+    }
+
+
+    @Get()
+    listFavorites() {
+        return this.favoritesService.listFavorites();
+    }
+
+    @Delete(":id")
+    removeFavorite(@Param('id') id: string): string {
+        this.favoritesService.removeFavorite(id);
+        return 'Movie removed from favorites';
+    }
+}
--- backend/src/favorites/favorites.service.ts
+++ backend/src/favorites/favorites.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from '../types/movie.type';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+    private favorites: any[] = [];
+
+    addFavorite(movie: Movie): void {
+        const isFavorite = this.favorites.find(favorite =>
+            favorite.imdbID === movie.imdbID
+        );
+        if (!isFavorite) {
+            this.favorites.push(movie);
+        }
+    }
+
+    listFavorites() {
+        return this.favorites;
+    }
+
+    removeFavorite(imdbID: string) {
+        const index = this.favorites.findIndex(favorite =>
+            favorite.imdbID === imdbID
+        );
+        if (index === -1) {
+            throw new NotFoundException('Movie not found in favorites');
+        }
+        this.favorites.splice(index, 1);
+        return this.favorites;
+    }
+}
