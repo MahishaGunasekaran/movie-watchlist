c1219360779386df044fd29a251a11f60297c0b5
Mahi-Mani <m.mahishamani@gmail.com>
2025-11-04 13:09:56 -0800

    Backend implementation of the recommendation module

--- backend/src/recommendations/recommendations.controller.ts
+++ backend/src/recommendations/recommendations.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { RecommendationsService } from './recommendations.service';
 
 @Controller('recommendations')
-export class RecommendationsController {}
+export class RecommendationsController {
+     constructor(private readonly recommendationsService: RecommendationsService) { }
+
+     @Post()
+     async getRecommendations(@Body("favorites") favorites: any[]) {
+        return this.recommendationsService.getRecommendations(favorites);
+     }
+}
