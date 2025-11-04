75def10517a8d1a9963f717cc981d1e5a52d60d9
Mahi-Mani <m.mahishamani@gmail.com>
2025-11-04 12:56:29 -0800

    Create recommendations module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { ConfigModule } from '@nestjs/config';
+import { RecommendationsModule } from './recommendations/recommendations.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
       isGlobal: true,
-    }), MoviesModule, FavoritesModule],
+    }), MoviesModule, FavoritesModule, RecommendationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
